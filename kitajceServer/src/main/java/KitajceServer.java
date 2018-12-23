2f4acc868b585cff272e09468f33a2284cadac7d
pawelrubin <pawelrubin19@gmail.com>
2018-12-23 23:14:19 +0100

    Update KitajceServer.java

--- kitajceServer/src/main/java/KitajceServer.java
+++ kitajceServer/src/main/java/KitajceServer.java
@@ -9,13 +9,24 @@ public class KitajceServer {
   private static final int port = 2137;
   private int numOfPlayers;
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     KitajceServer server = new KitajceServer();
-    server.numOfPlayers = Integer.parseInt(args[0]);
-    if (server.numOfPlayers < 2 || server.numOfPlayers > 6 || server.numOfPlayers == 5 ) {
+    if (args.length == 0) {
+      System.out.println("Too few arguments.");
       System.exit(1);
     }
-    server.start();
+    try {
+      server.numOfPlayers = Integer.parseInt(args[0]);
+      if (server.numOfPlayers < 2 || server.numOfPlayers > 6 || server.numOfPlayers == 5) {
+        throw new IllegalArgumentException("Illegal number of players.");
+      }
+      server.start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (IllegalArgumentException ex) {
+      System.out.println(ex.getMessage());
+      System.exit(1);
+    }
   }
 
   private void start() throws IOException {
